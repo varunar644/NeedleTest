f63611cd857b0ebb223fa03c1eaf04e03a159621
Varun AR <varun@tiltlabs.io>
2023-08-07 19:20:46 +0530

    * initial commit

---
+++ src/generated/gen.js
@@ -0,0 +1,17 @@
+﻿import "./register_types"
+
+export const needle_exported_files = [];
+globalThis["needle:codegen_files"] = needle_exported_files;
+needle_exported_files.push("assets/scene.glb?v=1691416141254");
+
+document.addEventListener("DOMContentLoaded", () =>
+{
+	const needleEngine = document.querySelector("needle-engine");
+	if(needleEngine && needleEngine.getAttribute("src") === null)
+	{
+		needleEngine.setAttribute("hash", "1691416141254");
+		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
+	}
+});
+
+console.log("Made\ with\ ♥\ by\ 🌵\ Needle\ -\ https://needle\.tools\ —\ Version\ 3\.10\.7-pre");
